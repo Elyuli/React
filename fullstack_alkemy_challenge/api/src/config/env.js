d690c2ba0714abb2c8a00f13df2f60c6ce299a4e
Elyuli <elyuli.dev@gmail.com>
2022-07-20 18:32:01 +0200

    Create logic of env

--- fullstack_alkemy_challenge/api/src/config/env.js
+++ fullstack_alkemy_challenge/api/src/config/env.js
@@ -1,5 +1,10 @@
 const env = {
+	HOST: process.env.HOST || "localhost",
 	PORT: process.env.PORT || "3000",
+	DATABASE: process.env.DATABASE || "alkemy",
+	USERNAME: process.env.USERNAME || "mysql",
+	PASSWORD: process.env.PASSWORD || "mysql",
+	DIALECT: process.env.DIALECT || "mysql",
 };
 
 export default env;
